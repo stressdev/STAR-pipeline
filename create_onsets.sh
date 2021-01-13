14aa7eae8315837e97115cd1fb668d362adf0546
John Flournoy <john_flournoy@fas.harvard.edu>
2021-01-13 14:50:36 -0500

    update

--- create_onsets.sh
+++ create_onsets.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+set -axo pipefail
+set +eu
 
 echo "Loading modules..."
 module load Anaconda3/2019.10 python/3.7.7-fasrc01
 source activate star
+
+set -eu
 
 echo "Running scripts in OnsetScripts/"
 for pyfile in `ls OnsetScripts/*py`; do
